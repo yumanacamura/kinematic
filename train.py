8f7cd7fdef91341e3dbe83ed34e8850e87fa6b98
yumanacamura <yuma.nacamura@gmail.com>
2019-01-10 00:11:01 +0900

    fix to move

--- train.py
+++ train.py
@@ -29,16 +29,16 @@ FILENAME = './model/kinect.model'
 
 k_input = Input((4,)) #x,y,sin(theta),cos(theta)
 
-model = Dense(256, activation='LeakyReLU')(model)
+model = Dense(256, activation='relu')(k_input)
 model = Dropout(0.25)(model)
-model = Dense(128, activation='LeakyReLU')(model)
+model = Dense(128, activation='relu')(model)
 model = Dropout(0.25)(model)
-model = Dense(32, activation='LeakyReLU')(model)
+model = Dense(32, activation='relu')(model)
 model = Dropout(0.25)(model)
-model = Dense(16, activation='LeakyReLU')(model)
+model = Dense(16, activation='relu')(model)
 model = Dropout(0.25)(model)
 
-model = Dense(3, activation='')(model)
+model = Dense(3, activation='linear')(model)
 
 kinect = Model(inputs=k_input, outputs=model)
 
@@ -46,27 +46,32 @@ kinect.summary()
 
 kinect.compile(loss='mean_squared_error',optimizer='adam')
 
-result = open('result.txt','w')
-for iteration in range(iterations):
-    theta = np.random.uniform(-90, 90, (batch_size,3))
-    s = np.sin(theta)
-    c = np.cos(theta)
+def theta2ans(theta):
+    s = np.sin(theta).T
+    c = np.cos(theta).T
     s12 = s[0]*c[1] + c[0]*s[1]
     c12 = c[0]*c[1] - s[0]*s[1]
     s123 = s12*c[2] + c12*s[2]
     c123 = c12*c[2] - s12*s[2]
     x = s[0] + s12 + s123
     y = c[0] + c12 + c123
-    th = theta.sum(axis=0)
+    th = theta.sum(axis=1)
 
-    ans = np.array(x,y,np.cos(th),np.sin(th)).T
+    return np.array([x,y,np.cos(th),np.sin(th)]).T
 
-    loss = predict_kigo.train_on_batch(ans,theta)
+for iteration in range(iterations):
+    theta = np.random.uniform(-np.pi/2, np.pi/2, (batch_size,3))
+    ans = theta2ans(theta)
+    loss = kinect.train_on_batch(ans,theta)
 
-    print("%d loss: %f, acc.: %.2f%%" % (iteration, loss[0], 100 * loss[1]))
-    if (iteration+1) % test_interval == 0:
-        result.write("%d loss: %f, acc.: %.2f%%" % (iteration, loss[0], 100 * score[1]))
-    if (iteration+1) % model_interval == 0:
-        predict_kigo.save("model_{0}_epechs.h5")
+test_size = 10
+theta = np.random.uniform(-np.pi/2, np.pi/2, (test_size,3))
+ans = theta2ans(theta)
 
-result.close()
+predict = kinect.predict(ans)
+ans2 = theta2ans(predict)
+
+for i in range(test_size):
+  print(i)
+  print('\t'.join(map(str,ans[i])))
+  print('\t'.join(map(str,ans2[i])))
